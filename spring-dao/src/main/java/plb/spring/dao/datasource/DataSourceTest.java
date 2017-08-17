9621b90efae033e877016a86c69686ff8b62ed1b
Panlibin2015 <18267568873@163.com>
2017-08-17 12:59:24 +0800

    update file

--- spring-dao/src/main/java/plb/spring/dao/datasource/DataSourceTest.java
+++ spring-dao/src/main/java/plb/spring/dao/datasource/DataSourceTest.java
@@ -5,11 +5,8 @@ import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.Properties;
 
-import javax.sql.DataSource;
-
 import org.apache.commons.dbcp.BasicDataSource;
 import org.junit.Test;
-import org.springframework.jndi.JndiObjectFactoryBean;
 
 public class DataSourceTest {
 	private Properties pop = new Properties();
